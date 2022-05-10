d096029ea2233b87f46c4ff66b630aefa7f65da0
hrbrmstr <bob@rud.is>
2022-05-10 10:13:38 -0400

    Initial commit

---
+++ R/extendr-wrappers.R
@@ -0,0 +1,8 @@
+#' @docType package
+#' @usage NULL
+#' @useDynLib minifyhtml, .registration = TRUE
+NULL
+
+#' Return string `"Hello world!"` to R.
+#' @export
+hello_world <- function() .Call(wrap__hello_world)
